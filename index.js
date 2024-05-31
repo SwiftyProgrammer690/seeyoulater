f8723da0588eb2743e1c2a0bc8381a557b49562d
Swarit Choudhari <swaritchoudhari.p.s.y@gmail.com>
2024-05-30 22:03:33 -0400

    until i see u again... 😭

--- index.js
+++ index.js
@@ -3,20 +3,27 @@ function validateForm() {
     if (pass === "") {
         alert("Password must be filled out");
         return false;
-    } else if (pass === "Dora_I$_a_bettername") {
-        console.log("What's good Nora");
+    } else if (pass === "Dora_I$_a_bettername") { // Nora
+        window.location.href = "ggwp/488498545798hdgyyd.html";
         return false;
-    } else if (pass === "thisONEasianP3rson") {
-        console.log("Leilah");
+    } else if (pass === "thisONEasianP3rson") { // leilah
+        window.location.href = "/ggwp/83453577vc988978.html";
         return false;
-    } else if (pass === "kindergartenbros") {
+    } else if (pass === "kindergartenbros") { // Matteo
         window.location.href = "/ggwp/0349673763578xxx68964.html";
         return false;
-    } else if (pass === "B3tter_than_swarit!") {
-        window.location.href = "/ggwp/7757877838794578334500xx.html"
+    } else if (pass === "B3tter_than_swarit!") { // Molly
+        window.location.href = "/ggwp/7757877838794578334500xx.html";
         return false;
-    } else if (pass == " "){
-        //f
+    } else if (pass == "C00l_dud3"){ // Marcus
+        window.location.href = "/ggwp/36789245769237589235375.html";
+        return false;
+    } else if (pass == "DrUMMA_B0i") { // Ethan
+        window.location.href = "/ggwp/112133212313215526636363.html";
+        return false;
+    } else if (pass == "S3ansun...") { // Sean
+        window.location.href = "/ggwp/289678935678924576fddgdsfgdfsdsssdfdgssdgfdgfs.html";
+        return false;
     } else {
         alert("Incorrect password");
         return false;
